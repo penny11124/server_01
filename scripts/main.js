01a7722d8e0c0a8b4f8504456aeec961c852e4eb
MIKE <6301cxutop@gmail.com>
2023-10-21 17:09:05 +0800

    test

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+import {
+    world,
+    system
+  } from "@minecraft/server";
+
+function getPlayer() {
+  const allPlayers = world.getAllPlayers();
+  if (allPlayers.length === 0) {
+    return undefined;
+  }
+
+  return allPlayers[0];
+}
+
+function getPlayerDimension() {
+  const player = getPlayer();
+  if (player === undefined) {
+    return undefined;
+  }
+  return player.dimension;
+}
+
+function getPlayerLocation() {
+  const player = getPlayer();
+  if (player === undefined) {
+    return undefined;
+  }
+  return player.location;
+}
+
+function mainTick() {
+  if (system.currentTick % 200 === 0) {
+    const playerDimension = getPlayerDimension();
+    const playerLocation = getPlayerLocation();
+    if (playerDimension !== undefined && playerLocation !== undefined) {
+      if (playerDimension.id === "minecraft:overworld") { 
+        playerDimension.spawnEntity("minecraft:dirt", playerLocation); 
+      } 
+      else if (playerDimension.id === "minecraft:nether") { 
+        playerDimension.spawnEntity("minecraft:hoglin", playerLocation); 
+      }
+      else {
+        playerDimension.spawnEntity("minecraft:wolf", playerLocation);
+      }
+    }
+  }
+  system.run(mainTick);
+}
+
+system.run(mainTick);
